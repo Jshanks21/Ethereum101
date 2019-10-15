59abe3dc7278c7b6b5d5e410e13f2bd2c2619cb7
Luis Fernando <lfegea@aol.com>
2019-10-15 07:40:47 -0700

    Structs Example
    Example of using a struct data type in Solidity. Also includes an opening time modifier so the contract can't be accessed until a set time.

---
+++ Structs.sol
@@ -0,0 +1,28 @@
+pragma solidity >=0.4.0 <0.7.0;
+
+contract MyContract {
+    uint256 public peopleCount = 0;
+    mapping(uint => Person) public people;
+    
+    uint256 openingTime = 1570634174;
+
+    modifier onlyWhileOpen() {
+        require(block.timestamp >= openingTime); //The time stampe of the current block compared to set epoch time
+        _;
+    }
+    
+    struct Person {
+        uint _id;
+        string _firstName;
+        string _lastName;
+    }
+    
+    function addPerson(string memory _firstName, string memory _lastName) public onlyWhileOpen {
+        incrementCount();
+        people[peopleCount] = Person(peopleCount, _firstName, _lastName);
+    }
+    
+    function incrementCount() internal {
+        peopleCount += 1;
+    }
+}
